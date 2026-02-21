3982f65e719162db20b6e17d2f25d5f7f9d3f22e
alexcl11 <aleecanovaslopez@gmail.com>
2026-02-21 11:37:08 +0100

    Añadir creación dinámica de columnas en tablas de empresa
    Se implementa la funcionalidad para crear nuevas columnas desde la interfaz, permitiendo definir nombre, tipo de dato, opciones (para "Select") o tabla relacionada (para "Relación").
    Se agregan métodos en el backend para gestionar la creación de columnas, inserción de opciones y obtención de datos para los formularios.
    La vista muestra chips de colores para "Select" y nombres reales para relaciones.
    Se añade un modal para crear columnas y scripts para su gestión.
    El botón "Añadir Columna" ahora abre el nuevo modal.

--- ProyectoSyncro/Controllers/DashboardController.cs
+++ ProyectoSyncro/Controllers/DashboardController.cs
@@ -20,15 +20,48 @@ namespace ProyectoSyncro.Controllers
                 List<Dictionary<string, object>> datos = 
                     await this.repo.GetDatosTablaEmpresaAsync(idEmpresa, tabla);
                 var columnas = await this.repo.GetColumnasTablaAsync(idEmpresa, tabla);
+                var opciones = await this.repo.GetOpcionesSelectTablaEmpresa(idEmpresa, tabla);
+                var relaciones = await this.repo.GetOpcionesRelacionTablaEmpresa(idEmpresa, tabla);
+                List<string> tablas = await this.repo.GetTablasEmpresaAsync(1);
 
+                ViewData["TablasEmpresa"] = tablas;
                 ViewData["Title"] = tabla;
                 ViewData["Columnas"] = columnas;
+                ViewData["OpcionesSelect"] = opciones;
+                ViewData["OpcionesRelacion"] = relaciones;
+                
                 return View(datos);
             } else
             {
                 return View();
             }
                 
+        }
+
+        public async Task<IActionResult> CreateColumn
+            (string nombreTabla, string nombreColumna, string tipoDato, string? nombreTablaRelacionada, 
+            List<string> opcionesValor, List<string> opcionesColor)
+        {
+            int idEmpresa = 1;
+
+            await this.repo.CreateColumnaTabla(idEmpresa, nombreTabla, nombreColumna, tipoDato, nombreTablaRelacionada);
+
+            if (tipoDato == "Select" && opcionesValor != null && opcionesValor.Count > 0)
+            {
+                for (int i = 0; i < opcionesValor.Count; i++)
+                {
+                    string valor = opcionesValor[i];
+
+                    if (!string.IsNullOrWhiteSpace(valor))
+                    {
+                        string color = (opcionesColor != null && opcionesColor.Count > i) ? opcionesColor[i] : "#64748b";
+                        await this.repo.InsertarOpcionColumnaAsync(idEmpresa, nombreTabla, nombreColumna, valor, color);
+                    }
+                }
+            }
+            
+
+            return RedirectToAction("Index", new { tabla = nombreTabla });
         }
 
         public async Task<IActionResult> CreateRegistro(string nombreTabla, Dictionary<string, string> valoresRegistro)
--- ProyectoSyncro/Repositories/BaseRepository.cs
+++ ProyectoSyncro/Repositories/BaseRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Microsoft.Extensions.Options;
 using ProyectoSyncro.Data;
 using ProyectoSyncro.Models;
 using System.Data.Common;
+using System.Net.NetworkInformation;
 using System.Text.Json;
 
 namespace ProyectoSyncro.Repositories
@@ -101,6 +103,78 @@ namespace ProyectoSyncro.Repositories
             }
         }
 
+        public async Task CreateColumnaTabla
+            (int idEmpresa, string nombreTabla, string nombreColumna, 
+            string tipoDato, string? nombreTablaRelacionada)
+        {
+            var idTablaRelacionada = 0;
+            if (nombreTablaRelacionada != null)
+            {
+                idTablaRelacionada = await (from datos in this.context.MetaTablas
+                                                where datos.IdEmpresa == idEmpresa &&
+                                                datos.Nombre == nombreTablaRelacionada
+                                                select datos.IdTabla).FirstOrDefaultAsync();
+            }
+            string sql = "SP_INSERTAR_COLUMNA_TABLA_SCHEMA_EMPRESA";
+            SqlParameter paramIdEmpresa = new SqlParameter("@IdEmpresa", idEmpresa);
+            SqlParameter paramNombreTabla = new SqlParameter("@nombreTabla", nombreTabla);
+            SqlParameter paramNombreColumna = new SqlParameter("@nombreColumna", nombreColumna);
+            SqlParameter paramTipoDato = new SqlParameter("@tipoDato", tipoDato);
+            SqlParameter paramIdTablaRelacion;
+            if (idTablaRelacionada == 0)
+            {
+                paramIdTablaRelacion = new SqlParameter("@idTablaRelacionada", null);
+            }
+            else
+            {
+                paramIdTablaRelacion = new SqlParameter("@idTablaRelacionada", idTablaRelacionada);
+            }
+
+
+                using (DbCommand com =
+                    this.context.Database.GetDbConnection().CreateCommand())
+                {
+                    com.CommandType = System.Data.CommandType.StoredProcedure;
+                    com.CommandText = sql;
+                    com.Parameters.Add(paramIdEmpresa);
+                    com.Parameters.Add(paramNombreTabla);
+                    com.Parameters.Add(paramNombreColumna);
+                    com.Parameters.Add(paramTipoDato);
+                    com.Parameters.Add(paramIdTablaRelacion);
+
+                    await com.Connection.OpenAsync();
+                    await com.ExecuteNonQueryAsync();
+                    await com.Connection.CloseAsync();
+                }
+        }
+
+        public async Task InsertarOpcionColumnaAsync(int idEmpresa, string nombreTabla, string nombreColumna, string valor, string color)
+        {
+
+            string sql = "SP_INSERTAR_OPCION_COLUMNA";
+            SqlParameter paramIdEmpresa = new SqlParameter("@IdEmpresa", idEmpresa);
+            SqlParameter paramNombreTabla = new SqlParameter("@NombreTabla", nombreTabla);
+            SqlParameter paramNombreColumna = new SqlParameter("@NombreColumna", nombreColumna);
+            SqlParameter paramValor= new SqlParameter("@Valor", valor);
+            SqlParameter paramColor = new SqlParameter("@Color", color);
+
+            using (var com = this.context.Database.GetDbConnection().CreateCommand())
+            {
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.CommandText = sql;
+
+                com.Parameters.Add(paramIdEmpresa);
+                com.Parameters.Add(paramNombreTabla);
+                com.Parameters.Add(paramNombreColumna);
+                com.Parameters.Add(paramValor);
+                com.Parameters.Add(paramColor); 
+
+                await com.Connection.OpenAsync();
+                await com.ExecuteNonQueryAsync();
+                await com.Connection.CloseAsync();
+            }
+        }
+
         public async Task<List<MetaColumna>> GetColumnasTablaAsync
             (int idEmpresa, string nombreTabla)
         {
@@ -133,6 +207,115 @@ namespace ProyectoSyncro.Repositories
                 await com.ExecuteNonQueryAsync();
                 await com.Connection.CloseAsync();
             }
+        }
+
+        public async Task<Dictionary<string, List<MetaOpcione>>> GetOpcionesSelectTablaEmpresa
+            (int idEmpresa, string nombreTabla)
+        {
+            Dictionary<string, List<MetaOpcione>> opciones = new Dictionary<string, List<MetaOpcione>>();
+
+            var idTablaConsulta = from datos in this.context.MetaTablas
+                           where datos.IdEmpresa == idEmpresa &&
+                           datos.Nombre == nombreTabla
+                           select datos.IdTabla;
+
+            int idTabla = await idTablaConsulta.FirstOrDefaultAsync();
+
+
+            var idColumnas = from datos in this.context.MetaColumnas
+                           where datos.IdTabla == idTabla &&
+                           datos.TipoDato == "Select"
+                           select datos;
+
+            List<MetaColumna> columnas = await idColumnas.ToListAsync();
+
+            if (columnas == null)
+            {
+                return null;
+            }
+
+            foreach (MetaColumna columna in columnas)
+            {
+                var opcionesColumna = await (from datos in this.context.MetaOpciones
+                                where datos.IdColumna == columna.IdColumna
+                                select datos).ToListAsync();
+
+                opciones.Add(columna.Nombre, opcionesColumna);
+            }
+
+            return opciones;
+
+        }
+
+        public async Task<Dictionary<string, Dictionary<string, string>>> GetOpcionesRelacionTablaEmpresa(int idEmpresa, string nombreTabla)
+        {
+            var opcionesRelacion = new Dictionary<string, Dictionary<string, string>>();
+
+            string nombreSchema = await (from datos in this.context.Empresas
+                                        where datos.IdEmpresa == idEmpresa
+                                        select datos.NombreSchema)
+                                        .FirstOrDefaultAsync();
+
+            var tablaActual = await (from datos in this.context.MetaTablas
+                                     where datos.IdEmpresa == idEmpresa &&
+                                     datos.Nombre == nombreTabla
+                                     select datos).FirstOrDefaultAsync();
+
+            if (tablaActual == null) 
+            {
+                return opcionesRelacion;
+            }
+
+            var columnasRelacion = await (from datos in this.context.MetaColumnas
+                                          where datos.IdTabla == tablaActual.IdTabla &&
+                                          datos.TipoDato == "Relacion" &&
+                                          datos.IdTablaRelacionada != null
+                                          select datos).ToListAsync();
+
+            foreach (var columna in columnasRelacion)
+            {
+                var diccionarioDatos = new Dictionary<string, string>();
+
+                var tablaVinculada = await (from datos in this.context.MetaTablas
+                                            where datos.IdTabla == columna.IdTablaRelacionada
+                                            select datos).FirstOrDefaultAsync();
+
+                if (tablaVinculada != null)
+                {
+                    var primeraColumna = await (from datos in this.context.MetaColumnas
+                                                where datos.IdTabla == tablaVinculada.IdTabla
+                                                orderby datos.IdColumna
+                                                select datos).FirstOrDefaultAsync();
+
+                    string nombreColumnaVisual = primeraColumna != null ? primeraColumna.Nombre : "Id";
+
+                    string sql = $"SELECT [Id], [{nombreColumnaVisual}] AS Mostrar FROM {nombreSchema}.[{tablaVinculada.Nombre}]"; 
+                    
+                    using (DbCommand com = this.context.Database.GetDbConnection().CreateCommand())
+                    {
+                        com.CommandType = System.Data.CommandType.Text;
+                        com.CommandText = sql;
+
+                        await com.Connection.OpenAsync();
+
+                        DbDataReader reader = await com.ExecuteReaderAsync();
+
+                        while (await reader.ReadAsync())
+                        {
+                            string idFila = reader["Id"].ToString();
+                            string nombreFila = reader["Mostrar"].ToString();
+
+                            diccionarioDatos.Add(idFila, nombreFila); 
+                        }
+                        await reader.CloseAsync();
+                        await com.Connection.CloseAsync();
+                    }
+                }
+
+                opcionesRelacion.Add(columna.Nombre, diccionarioDatos);
+            }
+            
+            return opcionesRelacion;
         }
 
     }
